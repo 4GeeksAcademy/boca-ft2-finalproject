b03f476b54a5d04e9d7da4b07569e40d24595ca6
Branden Reilly <155998782+brandenreilly@users.noreply.github.com>
2024-04-02 19:20:52 +0000

    added Models + 4-5 API routes for User features.

--- src/api/models.py
+++ src/api/models.py
@@ -3,19 +3,96 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    first_name = db.Column(db.String(80))
-    last_name = db.Column(db.String(80))
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    postal_code = db.Column(db.String(120), nullable=False)
+    dob = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
         return {
-            "id": self.id,
+            "uid": self.uid,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "postal_code": self.postal_code,
+            "dob": self.dob
             # do not serialize the password, its a security breach
         }
+    
+class UserPage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey(User.uid))
+    top_songs = db.Column(db.String(250))
+    top_artists = db.Column(db.String(250))
+    top_genres = db.Column(db.String(250))
+    attended_concerts = db.Column(db.String(250))
+    upcoming_concerts = db.Column(db.String(250))
+    about_me = db.Column(db.String(250))
+    prof_pic_url = db.Column(db.String(250))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "uid": self.uid,
+            "top_songs": self.top_songs,
+            "top_artists": self.top_artists,
+            "top_genres": self.top_genres,
+            "attended_concerts": self.attended_concerts,
+            "upcoming_concerts": self.upcoming_concerts,
+            "about_me": self.about_me,
+            "prof_pic_url": self.prof_pic_url
+        }
+    
+class Playlist(db.Model):
+    pid = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey(User.uid))
+    playlist_name = db.Column(db.String(120), nullable=False)
+    top_three = db.Column(db.String(120), nullable=True)
+
+class PlaylistSongs(db.Model):
+    psid = db.Column(db.Integer, primary_key=True)
+    pid = db.Column(db.Integer, db.ForeignKey(Playlist.pid))
+    song_name = db.Column(db.String(120))
+    song_genre = db.Column(db.String(120))
+    song_id = db.Column(db.Integer)
+    
+class Follower(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey(User.uid))
+    follower_id = db.Column(db.Integer, db.ForeignKey(User.uid))
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey(User.uid))
+    content = db.Column(db.String(250))
+    likes = db.Column(db.Integer)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey(User.uid))
+    comment_text = db.Column(db.String(250))
+    postid = db.Column(db.Integer, db.ForeignKey(Post.id))
+
+class Inbox(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.Text)
+    sender_id = db.Column(db.Integer, db.ForeignKey(User.uid))
+    recieved_id = db.Column(db.Integer, db.ForeignKey(User.uid))
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey(User.uid))
+    name = db.Column(db.String(120))
+    artist = db.Column(db.String(120))
+    venue = db.Column(db.String(120))
+    location = db.Column(db.String(120))
+    date = db.Column(db.String(120))
+    img_url = db.Column(db.String(120))
+    attended_with = db.Column(db.String(120))
--- src/api/routes.py
+++ src/api/routes.py
@@ -14,9 +14,53 @@ CORS(api)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
-
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
+
+@api.route('/createuser', methods=['POST'])
+def handle_create_user():
+    sent_user = request.json
+    new_user = User(email=sent_user['email'], username=sent_user['username'], password=sent_user['password'], first_name=sent_user['first_name'], last_name=sent_user['last_name'], postal_code=sent_user['postal_code'], dob=sent_user['dob'])
+    db.session.add(new_user)
+    db.session.commit()
+    get_new_user = User.query.filter_by(username=sent_user['username'], password=sent_user['password']).first()
+    if get_new_user:
+        to_send = get_new_user.serialize()
+        return jsonify(to_send), 200
+    else:
+        return 'UNKNOWN ERROR', 400
+    
+@api.route('/getuser', methods=['POST'])
+def handle_get_user():
+    sent_info = request.json
+    get_user = User.query.filter_by(username=sent_info['username'], password=sent_info['password'])
+    if get_user:
+        return_user = get_user.serialize()
+        return jsonify(return_user), 200
+    else:
+        return 'USER NOT FOUND PLEASE SIGN UP', 403
+    
+@api.route('/deleteuser/<uid>', methods=['DELETE'])
+def handle_delete_user(uid):
+    get_user_to_delete = User.query.filter_by(uid=uid).first()
+    db.session.delete(get_user_to_delete)
+    db.session.commit()
+    return jsonify('User has been deleted.'), 200
+
+#@api.route('/updateprofile', methods=['PUT'])
+#def handle_update_profile():
+#    sent_info = request.json
+#    current_user = User.query.filter_by(uid=sent_info['uid']).first()
+# I've put this on backlog for now, I believe once we have the front end profile page set up and know what we're displaying, we'll know what we can update.
+
+@api.route('/changepassword', methods=['PUT'])
+def handle_change_password():
+    sent_info = request.json
+    get_user = User.query.filter_by(uid=sent_info['uid']).first()
+    if get_user.password == sent_info['password']:
+        get_user.password = sent_info['newpassword']
+        return 'Password Updated', 200
+    else: 
+        return 'Incorrect Password', 401
