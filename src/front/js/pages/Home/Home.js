3eaf8f630320049cf9d26ddc8980d30f860c3cd7
Debbie Dávila <146867711+DebbieDavilaDev@users.noreply.github.com>
2024-04-08 19:43:33 +0000

    updates to forms

--- src/front/js/pages/Home/Home.js
+++ src/front/js/pages/Home/Home.js
@@ -7,51 +7,64 @@ import { Context } from "../../store/appContext";
 export const Home = () => {
     const { store, actions } = useContext(Context);
     const navigate = useNavigate()
-
+    const centerStyle = {
+        margin: '0 auto', textAlign: 'center', display: 'block', // This makes the label behave like a block-level element
+    }
     return (
-        <div className="container red-background" style={{ display: "flexbox" }}>
+        <div className="container red-background" style={{ margin: '0 auto', }}>
             <br />
             <br />
-            <br />
-            <br />
-            <form>
+
+            <form style={{ margin: '0 auto', }}>
+                <div class="form-group"  >
+                    <h1 style={{ textAlign: "center", fontVariant: 'small-caps' }}>Create New Account</h1>
+                    <br />
+                    <label for="exampleInputName" style={centerStyle}>USER NAME</label>
+                    <input type="user name" style={centerStyle} class="form-control w-50" id="exampleInputUserName1" placeholder="Music Buff🎶" />
+                </div>
+
+                <label for="exampleInputEmail1" style={centerStyle}>EMAIL</label>
+                <input type="email" class="form-control w-50" style={centerStyle} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your email" />
+                <small id="emailHelp" class="form-text text-muted" style={centerStyle}>We'll never share your email with anyone else.</small>
+
                 <div class="form-group">
-                    <h2 style={{ textAlign: "center", fontVariant: 'small-caps' }}>Create New Account</h2>
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <label for="exampleInputPassword1" style={centerStyle} >PASSWORD</label>
+                    <input type="password" style={centerStyle} class="form-control w-50" id="exampleInputPassword1" placeholder="Password" />
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    <label for="exampleInputZipCode1" style={centerStyle} >ZIP CODE</label>
+                    <input type="zipcode" style={centerStyle} class="form-control w-50" id="exampleInputZipCode1" placeholder="Zip Code" />
                 </div>
+
                 <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+
 
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-warning" style={centerStyle}>Sign up!</button>
             </form>
             <br />
             <br />
             <br />
-            <br />
+            <h3 style={{ textAlign: "center", fontVariant: 'small-caps' }}>Already Registered? Log in here.</h3>
             <br />
             <form>
-                <div class="form-group">
-                    <h2 style={{ textAlign: "center", fontVariant: 'small-caps' }}>Already Registered? Login Here. </h2>
-                    <label for="exampleInputEmail1">User Name</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username" />
-                    <small id="emailHelp" class="form-text text-muted">Welcome Back!</small>
+                <div class="form-group" style={{ display: 'flex', justifyContent: 'center' }}>
+                    <div style={{ marginRight: '10px' }}>
+
+                        <label for="exampleInputEmail1" style={centerStyle}>User Name</label>
+                        <input type="email" class="form-control w-80" style={centerStyle} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username" />
+                        <small id="emailHelp" class="form-text text-muted" style={centerStyle}>Welcome Back!</small>
+                    </div>
+                    <div>
+                        <label for="exampleInputPassword1" style={centerStyle}>Password</label>
+                        <input type="password" style={centerStyle} class="form-control w-80" id="exampleInputPassword1" placeholder="Password" />
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                </div>
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                <div class="form-check" style={centerStyle}>
+
 
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" style={centerStyle} class="btn btn-warning">Submit</button>
             </form>
         </div>
     );
