3de2b74f6084dfe7fd79f3722084cdb17903ebf2
Branden Reilly <155998782+brandenreilly@users.noreply.github.com>
2024-04-08 13:49:36 +0000

    added search other users

--- src/api/routes.py
+++ src/api/routes.py
@@ -111,5 +111,17 @@ def handle_track_concert():
 @api.route('/tracksong', methods=['POST'])
 def handle_track_song():
     sent_info = request.json
+
     TrackTopSongs.track_top_songs(sent_info['song_id'], sent_info['uid'])
     return jsonify('Tracked Song'), 200
+
+@api.route('/findothers', methods=['POST'])
+def handle_search():
+    recieved = request.json
+    search = "%{}%".format(recieved['name'])
+    find_user = User.query.filter(User.first_name.ilike(search) or User.username.ilike(search) or User.last_name.ilike(search)).all()
+    if find_user:
+        list_users = list(map(lambda x: x.serialize(), find_user))
+        return jsonify(list_users), 200
+    else:
+        return jsonify('User not found'), 404
