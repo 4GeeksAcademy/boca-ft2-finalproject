51bb2e86e115315f85450856b12f94eb322572ef
Debbie Dávila <146867711+DebbieDavilaDev@users.noreply.github.com>
2024-04-14 01:30:35 +0000

    changed login and signup page styling

--- src/front/js/pages/Login Page/Login.js
+++ src/front/js/pages/Login Page/Login.js
@@ -4,6 +4,7 @@ import { Navigate, useNavigate } from "react-router-dom"
 
 import { Context } from "../../store/appContext";
 import x from '../../../img/trumpets.jpg';
+import './Login.css'
 
 export const Login = () => {
 	const { store, actions } = useContext(Context);
@@ -12,25 +13,28 @@ export const Login = () => {
 	const [token, setToken] = useState(null)
 	const navigate = useNavigate()
 	const centerStyle = {
-		margin: '0 auto', textAlign: 'center', display: 'block', // This makes the label behave like a block-level element
+		margin: '0 auto', textAlign: 'center', display: 'block', color: 'black' // This makes the label behave like a block-level element
 	}
 	useEffect(() => { setToken(sessionStorage.getItem("token")) }, [store.user])
 	useEffect(() => { if (token) { navigate(`/home`) } }, [token])
 
 	return (
-		<section className="container_redkorn" style={{ backgroundImage: `url(${x})`, width: "100vw" }}>
+		<section className="container_redkorn" style={{ backgroundImage: `url(${x})`, width: "100vw", color: 'white' }}>
 			<div className="container red-background" style={{ margin: '0 auto', }}>
-
+				<h1 id="emailHelp" style={centerStyle}>Welcome Back!</h1>
+				<br />
+				<br />
 				<form>
-					<div class="form-group" style={{ display: 'flex', justifyContent: 'center' }}>
-						<div style={{ marginRight: '10px' }}>
+					<div class="form-group audiofont" style={{ display: 'flex', justifyContent: 'center' }}>
 
-							<label for="exampleInputEmail1" style={centerStyle}>User Name</label>
+						<div style={{ marginRight: '10px' }} className="audiofont">
+
+							<label for="exampleInputEmail1" style={centerStyle}>USERNAME</label>
 							<input onChange={(e) => { setUsernameInput(e.target.value) }} value={usernameInput} type="text" class="form-control w-80" style={centerStyle} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username" />
-							<small id="emailHelp" class="form-text text-muted" style={centerStyle}>Welcome Back!</small>
+
 						</div>
-						<div>
-							<label for="exampleInputPassword1" style={centerStyle}>Password</label>
+						<div className="audiofont">
+							<label className='audiofont' for="exampleInputPassword1" style={centerStyle}>PASSWORD</label>
 							<input onChange={(e) => { setPasswordInput(e.target.value) }} value={passwordInput} type="password" style={centerStyle} class="form-control w-80" id="exampleInputPassword1" placeholder="Password" />
 						</div>
 					</div>
